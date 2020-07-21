68bade4cf92f542e5dbab4f4832669757bb7fc4d
fuyuhaonabe <sls_asa_1206@yahoo.co.jp>
2020-07-21 22:50:46 +0900

    user登録後のページ遷移、profilesのバリデーション

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource) 
+    new_profile_path
+  end
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password])
   end
-
   
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,40 @@
 class Profile < ApplicationRecord
+
+  validates :family_name,
+  presence: true,                     # 必須
+  format: { with: /\A[ぁ-んァ-ン一-龥]/ } 
+
+  validates :first_name,
+  presence: true,
+  format: { with: /\A[ぁ-んァ-ン一-龥]/ } 
+
+  validates :family_name_kana,
+  presence: true,
+  format: { with: /\A[ァ-ヶー－]+\z/ } 
+
+  validates :first_name_kana,
+  presence: true,
+  format: { with: /\A[ァ-ヶー－]+\z/ }
+  
+  validates :birth,
+  presence: true,
+  format: { with: /\A[0-9]+\z/ }
+
+  validates :prefecuture,
+  presence: true,
+  format: { with: /\A[ぁ-んァ-ン一-龥]/ } 
+
+  validates :city,
+  presence: true,
+  format: { with: /\A[ぁ-んァ-ン一-龥]/ }   
+  
+  validates :address,
+  presence: true,
+  format: { with: /\A[ぁ-んァ-ン一-龥]/ } 
+
+  validates :telephone_number,
+  presence: true,
+  format: { with: /\A[0-9]+\z/ },
+
+
 end
