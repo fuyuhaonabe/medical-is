68f6989810c6ad7b1917d7d22e42dd9361f0434a
fuyuhaonabe <sls_asa_1206@yahoo.co.jp>
2020-07-21 22:24:39 +0900

    ユーザー登録実装（devise)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,
+  presence: true,                     # 必須
+  uniqueness: true,                   # 一意性
+  length: { maximum: 20 }            # あんまり長いのも
+
+  validates :email,
+  presence: true,
+  uniqueness: true,
+  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }  # メールアドレスの正規表現
+
+  validates :password,
+  presence: true,                     # 必須
+  length: { minimum: 7 },            # 7文字以上
+  format: { with: /\A[a-z0-9]+\z/i }  # 半角英数字のみ
 end
