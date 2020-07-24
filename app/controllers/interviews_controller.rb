d1dc9e9e59d358188ea3b4ff556c17246bffca29
fuyuhaonabe <sls_asa_1206@yahoo.co.jp>
2020-07-24 12:34:18 +0900

    interviewフォームのバックエンド実装

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,7 +1,58 @@
 class InterviewsController < ApplicationController
+  before_action :medical_history_string, only: [:create, :update]
+  before_action :life_details_string, only: [:create, :update]
+  before_action :dosage_form_string, only: [:create, :update]
+
   def index
+  end
+
+  def new
+    @interview = Interview.new
+  end
+
+  def create
+    @interview = Interview.new(interview_params)
+    unless @interview.valid?
+      render :new and return
+    end
+    @interview.save
+    redirect_to  complete_interviews_path(@user) and return
+  end
+
+  def medical_history_string
+    params[:interview][:medical_history] = params[:interview][:medical_history].join("、")  # to string
+  end
+
+  def life_details_string
+    params[:interview][:life_details] = params[:interview][:life_details].join("、")  # to string
+  end
+
+  def dosage_form_string
+    params[:interview][:dosage_form] = params[:interview][:dosage_form].join("、")  # to string
+  end
+
+  private
+
+  def interview_params
+    params.require(:interview).permit(
+      :diagnosis, 
+      :se_medicine,
+      :se_symptom,
+      :allergy,
+      :supplement,
+      :drinking,
+      :smoking,
+      :pregnancy,
+      :breastfeeding,
+      :child_weight,
+      :generic,
+      :dosage_form,
+      :life_details,
+      :medical_history,
+      :medical_history_etc).merge(user_id: current_user.id)
   end
 
   def complete
   end
+
 end
--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,2 +1,4 @@
 class Interview < ApplicationRecord
+  validates :generic,
+  presence: true
 end
